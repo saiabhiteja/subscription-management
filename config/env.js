96f2e1f90a5ef54df053f0471575ccae365f2641
saiabhiteja <saiabhiteja29@gmail.com>
2025-03-16 12:04:29 +0530

    added workflow and nodemailer

--- config/env.js
+++ config/env.js
@@ -2,4 +2,4 @@ import { config } from "dotenv";
 
 config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
-export const {PORT, NODE_ENV, DB_URI} = process.env
+export const {PORT, SERVER_URL, NODE_ENV, DB_URI, JWT_SECRET, JWT_EXPIRES_IN, ARCJET_KEY, ARCJET_ENV, QSTASH_TOKEN, QSTASH_URL, EMAIL_PASSWORD} = process.env
